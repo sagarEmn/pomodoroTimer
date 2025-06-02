dd3ee78f4fbb09fdeb4d5107bba8c87612470174
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-06-02 19:36:01 +0545

    feat: Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-6xl h-full w-full font-extrabold text-center flex justify-center bg-slate-900 text-white">
+      PomodoroTimer
+    </div>
+  );
+}
+
+export default App;
