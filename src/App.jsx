4e87697bb10d467ebf9937dee5124180e639d9c8
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-06-06 20:38:45 +0545

    fix: Color variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
   return (
-    <div className="text-6xl h-full w-full font-extrabold items-center bg-slate-900 text-white flex">
+    <div className="text-6xl h-full w-full font-extrabold items-center bg-slate-900 text-white flex ">
       <h1 className="max-w-6xl mx-auto text-center">PomodoroTimer</h1>
+      <div className="border-2 border-blue-400">
+        Clock
+      </div>
     </div>
   );
 }
